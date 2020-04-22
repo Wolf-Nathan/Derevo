3b589a6f71ecc92a458423ee0e7bb6598666ce15
Wolf Nathan <nathan-w@hotmail.fr>
2020-04-22 15:27:33 +0200

    Make road to files with node to transfer localStorage in each page

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 }).get('/add', function (req, res) {
     res.setHeader('Content-Type', 'text/html; charset=UTF-8');
     res.status(200);
-    res.sendFile('./addUser.html', {
+    res.sendFile('./views/addPerson.html', {
         root: __dirname
     });
 }).get('/edit:id', function (req, res) {
@@ -30,4 +30,45 @@ app.get('/', function (req, res) {
     res.sendFile('./infos.html?userId=' + req.params.id, {
         root: __dirname
     });
+})
+
+.get('/about', function (req, res) {
+    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+    res.status(200);
+    res.sendFile('./views/about.html', {
+        root: __dirname
+    });
+})
+
+.get('/css', function(req, res) {
+    res.setHeader('Content-Type', 'text/css; charset=UTF-8');
+    res.status(200);
+    res.sendFile('./dev/css/style.css', {root: __dirname});
+})
+
+.get('/font', function(req, res) {
+    res.setHeader('Content-Type', 'text/css; charset=UTF-8');
+    res.status(200);
+    res.sendFile('./dev/css/font.css', {root: __dirname });
+})
+
+.get('/vue', function(req, res) {
+    res.setHeader('Content-Type', 'application/javascript; charset=UTF-8');
+    res.status(200);
+    res.sendFile('./dev/js/vue.js', {root: __dirname });
+})
+
+.get('/pagination', function(req, res) {
+    res.setHeader('Content-Type', 'application/javascript; charset=UTF-8');
+    res.status(200);
+    res.sendFile('./dev/js/pagination.js', {root: __dirname });
+})
+
+//Il faudrait faire un truc qui permette de charger l'image voulue dynamiquement
+.get('/image1', function(req, res) {
+    res.setHeader('Content-Type', "image/jpg");
+    res.status(200);
+    res.sendFile('./assets/images/balzac.jpg',
+        {root: __dirname });
 });
+
