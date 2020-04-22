85f9d5e619efa608104d54d9d0aaea64c7a52c9a
Wolf Nathan <nathan-w@hotmail.fr>
2020-04-22 15:52:27 +0200

    Correct images problem

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.listen(port, function () {
     console.log('la machine est en route sur le port ' + port);
 })
 
+app.use('/images', express.static('./assets/images/'));
+
 app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'text/html; charset=UTF-8');
     res.status(200);
@@ -68,13 +70,5 @@ app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'application/javascript; charset=UTF-8');
     res.status(200);
     res.sendFile('./dev/js/pagination.js', {root: __dirname });
-})
-
-//Il faudrait faire un truc qui permette de charger l'image voulue dynamiquement
-.get('/image1', function(req, res) {
-    res.setHeader('Content-Type', "image/jpg");
-    res.status(200);
-    res.sendFile('./assets/images/balzac.jpg',
-        {root: __dirname });
 });
 
