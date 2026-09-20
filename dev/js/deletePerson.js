468b7e57eba2d81ed7c03e8b1f02c6ad1caa45da
Wolf-Nathan <nathan-w@hotmail.fr>
2026-09-20 15:45:41 +0200

    Fix personDel not set as global value

--- dev/js/deletePerson.js
+++ dev/js/deletePerson.js
@@ -10,6 +10,7 @@ let deleteModele = new Vue({
         nom: null,
         prenom: null,
         persons: null,
+        personDel: null,
     },
     components: {
         vuejsDatepicker
@@ -20,10 +21,10 @@ let deleteModele = new Vue({
                 this.persons = JSON.parse(localStorage.getItem('persons'));
                 const urlParams = new URLSearchParams(window.location.search);
                 this.id = Number(urlParams.get('id'));
-                let personDel = this.persons[this.id];
-                if (personDel && personDel.nom) {
-                    this.nom = personDel.nom;
-                    this.prenom = personDel.prenom;
+                this.personDel = this.persons[this.id];
+                if (this.personDel?.nom) {
+                    this.nom = this.personDel.nom;
+                    this.prenom = this.personDel.prenom;
                 }
                 else {
                     this.goodId = false;
@@ -38,16 +39,16 @@ let deleteModele = new Vue({
     },
     methods: {
         deleteYes: function () {
-            if (personDel.pere !== null) {
-                var position = this.persons[personDel.pere].enfants.indexOf(this.id);
-                this.persons[personDel.pere].enfants.splice(position, 1);
+            if (this.personDel.pere !== null) {
+                var position = this.persons[this.personDel.pere].enfants.indexOf(this.id);
+                this.persons[this.personDel.pere].enfants.splice(position, 1);
             }
-            if (personDel.mere !== null) {
-                var position = this.persons[personDel.mere].enfants.indexOf(this.id);
-                this.persons[personDel.mere].enfants.splice(position, 1);
+            if (this.personDel.mere !== null) {
+                var position = this.persons[this.personDel.mere].enfants.indexOf(this.id);
+                this.persons[this.personDel.mere].enfants.splice(position, 1);
             }
-            personDel.enfants.forEach(enfantId => {
-                if(personDel.sexe === 'H'){
+            this.personDel.enfants.forEach(enfantId => {
+                if(this.personDel.sexe === 'H'){
                     this.persons[enfantId].pere = null;
                 }
                 else {
@@ -56,7 +57,7 @@ let deleteModele = new Vue({
             });
 
             // On parcourt tout les mariages de la personne pour supprimer les mariages concernés chez les mariés.
-            personDel.mariages.forEach(mariage => {
+            this.personDel.mariages.forEach(mariage => {
                 // On récupère la personne mariée avec la personne supprimée.
                 const mary = this.persons[mariage.maryId];
                 // On supprime tout les mariages qui ont été faits avec la personne supprimée.
@@ -64,11 +65,11 @@ let deleteModele = new Vue({
                 this.persons[mary.id] = mary;
             });
 
-            personDel = {
+            this.personDel = {
                 id: -1
             };
 
-            this.persons[this.id] = personDel;
+            this.persons[this.id] = this.personDel;
 
             const parsed = JSON.stringify(this.persons);
             localStorage.setItem("persons", parsed);
